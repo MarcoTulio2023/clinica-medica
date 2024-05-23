30ab5661b5042843e79e6a17112df99f6ce1299d
vitoraugustobarcelospovoa <vitoraugusto15012005@gmail.com>
2024-05-23 11:06:24 -0300

    Para nâo perder progresso

--- clinica-administrativo/src/main/java/br/edu/imepac/models/MedicoModel.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/models/MedicoModel.java
@@ -1,5 +1,18 @@
 package br.edu.imepac.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "medicos")
 public class MedicoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String crm;
+
 }
