5838b78befc09abb8b6227d8cba926f8565d965d
HugoMartinsB <hugo.brito@aluno.imepac.edu.br>
2024-06-05 22:07:03 -0300

    codigo corrijido

---
+++ clinica-administrativo/src/main/java/br/edu/imepac/services/ConvenioService.java
@@ -0,0 +1,97 @@
+package br.edu.imepac.services;
+
+import br.edu.imepac.dtos.ConvenioCreateRequest;
+import br.edu.imepac.dtos.ConvenioDto;
+import br.edu.imepac.models.ConvenioModel;
+import br.edu.imepac.repositories.ConvenioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ConvenioService {
+
+    @Autowired
+    private ConvenioRepository convenioRepository;
+
+    public void delete(Long id) {
+        convenioRepository.deleteById(id);
+    }
+
+    public List<ConvenioDto> findAll() {
+        List<ConvenioModel> convenios = convenioRepository.findAll();
+        return convenios.stream().map(convenio -> {
+            ConvenioDto convenioDto = new ConvenioDto();
+            convenioDto.setId(convenio.getId());
+            convenioDto.setNome(convenio.getNome());
+            convenioDto.setTipoPlano(convenio.getTipoPlano());
+            convenioDto.setCobertura(convenio.getCobertura());
+            convenioDto.setInformacoesContato(convenio.getInformacoesContato());
+            return convenioDto;
+        }).collect(Collectors.toList());
+    }
+
+    public ConvenioDto update(Long id, ConvenioDto convenioDetails) {
+        Optional<ConvenioModel> optionalConvenio = convenioRepository.findById(id);
+
+        if (optionalConvenio.isPresent()) {
+            ConvenioModel convenioModel = optionalConvenio.get();
+            convenioModel.setNome(convenioDetails.getNome());
+            convenioModel.setTipoPlano(convenioDetails.getTipoPlano());
+            convenioModel.setCobertura(convenioDetails.getCobertura());
+            convenioModel.setInformacoesContato(convenioDetails.getInformacoesContato());
+
+            ConvenioModel updatedConvenio = convenioRepository.save(convenioModel);
+
+            ConvenioDto convenioDto = new ConvenioDto();
+            convenioDto.setId(updatedConvenio.getId());
+            convenioDto.setNome(updatedConvenio.getNome());
+            convenioDto.setTipoPlano(updatedConvenio.getTipoPlano());
+            convenioDto.setCobertura(updatedConvenio.getCobertura());
+            convenioDto.setInformacoesContato(updatedConvenio.getInformacoesContato());
+
+            return convenioDto;
+        } else {
+            return null;
+        }
+    }
+
+    public ConvenioDto save(ConvenioCreateRequest convenioRequest) {
+        ConvenioModel convenioModel = new ConvenioModel();
+        convenioModel.setNome(convenioRequest.getNome());
+        convenioModel.setTipoPlano(convenioRequest.getTipoPlano());
+        convenioModel.setCobertura(convenioRequest.getCobertura());
+        convenioModel.setInformacoesContato(convenioRequest.getInformacoesContato());
+
+        ConvenioModel savedConvenio = convenioRepository.save(convenioModel);
+
+        ConvenioDto convenioDto = new ConvenioDto();
+        convenioDto.setId(savedConvenio.getId());
+        convenioDto.setNome(savedConvenio.getNome());
+        convenioDto.setTipoPlano(savedConvenio.getTipoPlano());
+        convenioDto.setCobertura(savedConvenio.getCobertura());
+        convenioDto.setInformacoesContato(savedConvenio.getInformacoesContato());
+
+        return convenioDto;
+    }
+
+    public ConvenioDto findById(Long id) {
+        Optional<ConvenioModel> optionalConvenio = convenioRepository.findById(id);
+        if (optionalConvenio.isPresent()) {
+            ConvenioModel convenioModel = optionalConvenio.get();
+            ConvenioDto convenioDto = new ConvenioDto();
+            convenioDto.setId(convenioModel.getId());
+            convenioDto.setNome(convenioModel.getNome());
+            convenioDto.setTipoPlano(convenioModel.getTipoPlano());
+            convenioDto.setCobertura(convenioModel.getCobertura());
+            convenioDto.setInformacoesContato(convenioModel.getInformacoesContato());
+            return convenioDto;
+        } else {
+            return null;
+        }
+    }
+}
+
---
+++ clinica-administrativo/src/main/java/br/edu/imepac/services/EspecialidadeService.java
@@ -0,0 +1,84 @@
+package br.edu.imepac.services;
+
+import br.edu.imepac.dtos.EspecialidadeCreateRequest;
+import br.edu.imepac.dtos.EspecialidadeDto;
+import br.edu.imepac.models.EspecialidadeModel;
+import br.edu.imepac.repositories.EspecialidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class EspecialidadeService {
+
+    @Autowired
+    private EspecialidadeRepository especialidadeRepository;
+
+    public void delete(Long id) {
+        especialidadeRepository.deleteById(id);
+    }
+
+    public List<EspecialidadeDto> findAll() {
+        List<EspecialidadeModel> especialidades = especialidadeRepository.findAll();
+        return especialidades.stream().map(especialidade -> {
+            EspecialidadeDto especialidadeDto = new EspecialidadeDto();
+            especialidadeDto.setId(especialidade.getId());
+            especialidadeDto.setNome(especialidade.getNome());
+            especialidadeDto.setDescricao(especialidade.getDescricao());
+            return especialidadeDto;
+        }).collect(Collectors.toList());
+    }
+
+    public EspecialidadeDto update(Long id, EspecialidadeDto especialidadeDetails) {
+        Optional<EspecialidadeModel> optionalEspecialidade = especialidadeRepository.findById(id);
+
+        if (optionalEspecialidade.isPresent()) {
+            EspecialidadeModel especialidadeModel = optionalEspecialidade.get();
+            especialidadeModel.setNome(especialidadeDetails.getNome());
+            especialidadeModel.setDescricao(especialidadeDetails.getDescricao());
+
+            EspecialidadeModel updatedEspecialidade = especialidadeRepository.save(especialidadeModel);
+
+            EspecialidadeDto especialidadeDto = new EspecialidadeDto();
+            especialidadeDto.setId(updatedEspecialidade.getId());
+            especialidadeDto.setNome(updatedEspecialidade.getNome());
+            especialidadeDto.setDescricao(updatedEspecialidade.getDescricao());
+
+            return especialidadeDto;
+        } else {
+            return null;
+        }
+    }
+
+    public EspecialidadeDto save(EspecialidadeCreateRequest especialidadeRequest) {
+        EspecialidadeModel especialidadeModel = new EspecialidadeModel();
+        especialidadeModel.setNome(especialidadeRequest.getNome());
+        especialidadeModel.setDescricao(especialidadeRequest.getDescricao());
+
+        EspecialidadeModel savedEspecialidade = especialidadeRepository.save(especialidadeModel);
+
+        EspecialidadeDto especialidadeDto = new EspecialidadeDto();
+        especialidadeDto.setId(savedEspecialidade.getId());
+        especialidadeDto.setNome(savedEspecialidade.getNome());
+        especialidadeDto.setDescricao(savedEspecialidade.getDescricao());
+
+        return especialidadeDto;
+    }
+
+    public EspecialidadeDto findById(Long id) {
+        Optional<EspecialidadeModel> optionalEspecialidade = especialidadeRepository.findById(id);
+        if (optionalEspecialidade.isPresent()) {
+            EspecialidadeModel especialidadeModel = optionalEspecialidade.get();
+            EspecialidadeDto especialidadeDto = new EspecialidadeDto();
+            especialidadeDto.setId(especialidadeModel.getId());
+            especialidadeDto.setNome(especialidadeModel.getNome());
+            especialidadeDto.setDescricao(especialidadeModel.getDescricao());
+            return especialidadeDto;
+        } else {
+            return null;
+        }
+    }
+}
